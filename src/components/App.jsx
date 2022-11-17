e358d2adde748b130c2ddb04b0c972c2640d82f6
Inna <isko.inna.94@gmail.com>
2022-11-17 21:17:36 +0200

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,27 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
+
+  getInputValue = event => {
+    this.setState({ name: event.currentTarget.value });
+  };
+  renderMarcup = event => {
+    event.preventDefault();
+
+    this.setState(prevState => {
+      return { contacts: [...prevState.contacts, this.state.name] };
+    });
+    this.setState({ name: '' });
+  };
+
   render() {
     return (
-      <form action="">
+      <form action="" onSubmit={this.renderMarcup}>
         <label htmlFor="">
           Name
           <input
@@ -16,9 +30,16 @@ export class App extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            value={this.state.name}
+            onChange={this.getInputValue}
           />
         </label>
-        <button>Add Contact</button>
+        <button type="submit">Add Contact</button>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li>{this.state.contacts}</li>
+          ))}
+        </ul>
       </form>
     );
   }
